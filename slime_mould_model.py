0d13ccad440d13c3bcd3c3f8dfeb59f98416b8d6
aichinweze <aichinweze@gmail.com>
2026-01-27 08:18:32 -0500

    Updating model to use graph visualisation

--- slime_mould_model.py
+++ slime_mould_model.py
@@ -1,6 +1,49 @@
-from logging import getLevelNamesMapping
-
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def update_conductivity(edges_matrix, conductivity_matrix, start, end, r=1, inverse_length=None):
+    if inverse_length is None:
+        # TODO: Will need to be updated when lengths are variable
+        inverse_length = np.where(edges_matrix == 0, 0, 1 / edges_matrix)
+
+    conductivity_by_length = conductivity_matrix * inverse_length
+
+    reverse_conductivity = np.where(conductivity_by_length != 0, -conductivity_by_length, conductivity_by_length)
+
+    row_sums = np.sum(reverse_conductivity, axis=1) * -1
+
+    diagonal_indices = np.diag_indices_from(reverse_conductivity)
+
+    reverse_conductivity[diagonal_indices] = row_sums
+
+    pressure_sink_node = end
+
+    flow_vector = np.zeros(number_of_nodes)
+    flow_vector[start] = -1
+    flow_vector[end] = 1
+
+    reduced_flow_vector = np.delete(flow_vector, pressure_sink_node, axis=0)
+
+    reduced_conductivity_removed_row = np.delete(reverse_conductivity, pressure_sink_node, axis=0)
+    reduced_conductivity = np.delete(reduced_conductivity_removed_row, pressure_sink_node, axis=1)
+
+    inverse_conductivity = np.linalg.inv(reduced_conductivity)
+    pressure_vector = np.matmul(inverse_conductivity, reduced_flow_vector)
+
+    complete_pressure_vector = np.insert(pressure_vector, pressure_sink_node, 0)
+
+    flux = np.zeros_like(conductivity_by_length)
+
+    for idx, element in np.ndenumerate(conductivity_by_length):
+        if element != 0:
+            (row, col) = idx
+            flux[idx] = abs(element * (complete_pressure_vector[row] - complete_pressure_vector[col]))
+
+    derivative = flux - r * conductivity_matrix
+
+    return conductivity_matrix + derivative
+
 
 edges = np.array(
         [[0, 1, 0, 0],
@@ -9,112 +52,35 @@ edges = np.array(
          [0, 1, 0, 0]]
 )
 
-# TODO: Will need to be updated when lengths are variable
-inverse_length = np.where(edges == 0, 0, 1/edges)
+start_node = 0
+end_node = 2
 
-number_of_nodes = 4
+number_of_nodes = edges.shape[0]
 initialiser_rows = number_of_nodes
 initialiser_cols = number_of_nodes
 
 initialiser = np.array([[1 for _ in range(initialiser_cols)] for _ in range(initialiser_rows)])
 
-conductivity = edges * initialiser
-print(conductivity)
+initial_conductivity = edges * initialiser
 
-conductivity_by_length = conductivity * inverse_length
+G = nx.Graph(edges)
+pos = nx.spring_layout(G, seed=2797)
 
-reverse_conductivity = np.where(conductivity_by_length != 0, -conductivity_by_length, conductivity_by_length)
+last_conductivity = initial_conductivity
+for i in range(3):
+    edge_weights = [float(last_conductivity[u][v]) * 10 for u, v in G.edges()]
+    nx.draw_networkx_nodes(G, pos, node_color='black', node_size=500)
+    nx.draw_networkx_nodes(G, pos, nodelist=[start_node], node_color='red', node_size=500)
+    nx.draw_networkx_nodes(G, pos, nodelist=[end_node], node_color='green', node_size=500)
+    nx.draw_networkx_edges(G, pos, width=edge_weights, edge_color='gray', alpha=0.3)
 
-row_sums = np.sum(reverse_conductivity, axis=1) * -1
+    plt.show()
+    updated_conductivity = update_conductivity(edges, last_conductivity, start_node, end_node)
 
-diagonal_indices = np.diag_indices_from(reverse_conductivity)
+    if np.array_equal(last_conductivity, updated_conductivity):
+        break
+    else:
+        last_conductivity = updated_conductivity
 
-reverse_conductivity[diagonal_indices] = row_sums
-
-# print(reverse_conductivity)
-
-start_node = 0
-end_node = 2
-
-pressure_sink_node = 2
-
-flow_vector = np.zeros(number_of_nodes)
-flow_vector[start_node] = -1
-flow_vector[end_node] = 1
-# print(flow_vector)
-
-reduced_flow_vector = np.delete(flow_vector, pressure_sink_node, axis=0)
-# print(reduced_flow_vector)
-
-reduced_conductivity_removed_row = np.delete(reverse_conductivity, pressure_sink_node, axis=0)
-reduced_conductivity = np.delete(reduced_conductivity_removed_row, pressure_sink_node, axis=1)
-
-# print(reduced_conductivity)
-
-inverse_conductivity = np.linalg.inv(reduced_conductivity)
-pressure_vector = np.matmul(inverse_conductivity, reduced_flow_vector)
-
-# print(pressure_vector)
-
-complete_pressure_vector = np.insert(pressure_vector, pressure_sink_node, 0)
-print(complete_pressure_vector)
-
-flux = np.zeros_like(conductivity_by_length)
-
-for idx, element in np.ndenumerate(conductivity_by_length):
-    if element != 0:
-        (row, col) = idx
-        flux[idx] = abs(element * (complete_pressure_vector[row] - complete_pressure_vector[col]))
-
-print(flux)
-
-r = 1
-derivative = flux - r*conductivity
-print(derivative)
-
-node_coords = {0: (4, 0), 1: (2, 0), 2: (2, 2), 3: (0, 0)}
-tube_coords = {0: (3, 0), 1: (1, 0), 2: (2, 1)}
-
-tube_mapping = {0: (0, 1), 1: (1, 3), 2: (1, 2)}
-
-rows = 5
-cols = 3
-
-grid = np.full((rows, cols), "-", dtype=object)
-
-for key, value in node_coords.items():
-    grid[value] = "N"
-
-for key, value in tube_coords.items():
-    tube_value = str(conductivity[tube_mapping[key]])
-    grid[value] = tube_value
-
-print(grid)
-
-
-updated_conductivity = conductivity + derivative
-
-grid = np.full((rows, cols), "-", dtype=object)
-
-for key, value in node_coords.items():
-    grid[value] = "N"
-
-for key, value in tube_coords.items():
-    tube_value = str(updated_conductivity[tube_mapping[key]])
-    grid[value] = tube_value
-
-print(grid)
-
-
-
-# def print_grid(conductivity_matrix, node_coords, tube_coords, tube_mapping):
-#     rows = 5
-#     cols = 3
-#
-#     grid = np.full((rows, cols), "X", dtype=object)
-#
-#     for key, value in node_coords.items():
-#         grid[value] = "O"
-#
 
 
