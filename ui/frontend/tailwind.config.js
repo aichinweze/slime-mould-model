c2d40cad7db15fd6c747ea08eaff876a007cb123
aichinweze <aichinweze@gmail.com>
2026-03-24 12:33:55 -0400

    UI Frontend - Adding configure screen - currency tables.

--- ui/frontend/tailwind.config.js
+++ ui/frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#059b02",
+      },
+    },
   },
   plugins: [],
 };
